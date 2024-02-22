9b20d7c698e8f3f4404bb29158d533a925324d51
szturmi2137 <0jan0walczak0@gmail.com>
2024-02-22 20:17:53 +0100

    rejstracjastyl

--- rejestracja.php
+++ rejestracja.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MECHANIK Z PASJI</title>
+    <link rel="stylesheet" href="stylesheet/stylerejestracja.css">
+</head>
+<body>
 <?php
 session_start();
 session_destroy();
@@ -13,3 +22,5 @@ session_destroy();
 <p>Hasło:<br /><input type="password" id="haslo"/></p>
 <p>Potwierdź hasło:<br /><input type="password" id="powtwierdzhaslo"/></p>
 <button onclick="rejestracja()">Zarejestruj</button>
+</body>
+</html>
