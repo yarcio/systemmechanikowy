9ceecd0156de4197605bf1d2fcdda07e3578ac9c
szturmi2137 <0jan0walczak0@gmail.com>
2024-02-23 19:35:18 +0100

    siki

--- klient/index.php
+++ klient/index.php
@@ -2,7 +2,7 @@
 include $_SERVER["DOCUMENT_ROOT"]."/php/main.php";
 checklogin("k");
 ?>
-klient
+<h1>klient</h1>
 <script src="/js/main.js"></script>
 <div id="data"></div>
 <script>initKlient();</script>
