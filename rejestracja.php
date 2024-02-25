3e0c92a11627aa9ac169f962e2cdaf752340387c
szturmi2137 <0jan.walczak0@gmail.com>
2024-02-25 12:02:19 +0100

    poprawki

--- rejestracja.php
+++ rejestracja.php
@@ -1,28 +1,34 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mechanik z pasji</title>
     <link rel="stylesheet" href="stylesheet/stylerejestracja.css">
 </head>
-<body>
-<?php
-session_start();
-session_destroy();
-?>
-<h1>Mechanik z pasji</h1>
 
-<script src="js/main.js"></script>
-<p>Imie:<br /><input type='text' id='imie'/></p>
-<p>Nazwisko:<br /><input type='text' id='nazwisko'/></p>
-<p>Data urodzenia:<br /><input type='date' id='dataUr'/></p>
-<p>Numer telefonu:<br /><input type='text' id='nrTel'/></p>
-<p>Płeć: <output id="jakaplec">0</output><br>
-    K<input id="plec" type="range" min="-1" max="1" step="0.01" oninput="sliderRefresh()" value=0>M</p>
-<p>Nazwa użytkownika:<br /><input type='text' id='nazwa'/></p>
-<p>Hasło:<br /><input type="password" id="haslo"/></p>
-<p>Potwierdź hasło:<br /><input type="password" id="powtwierdzhaslo"/></p>
-<button onclick="rejestracja()">Zarejestruj</button>
+<body>
+    <?php
+    session_start();
+    session_destroy();
+    ?>
+    <h1>ZAREJESTRUJ SIĘ</h1>
+    <script src="js/main.js"></script>
+    <div class="polowa">
+        <p>Imie:<br /><input type='text' id='imie' /></p>
+        <p>Nazwisko:<br /><input type='text' id='nazwisko' /></p>
+        <p>Data urodzenia:<br /><input type='date' id='dataUr' /></p>
+        <p>Numer telefonu:<br /><input type='text' id='nrTel' /></p>
+    </div>
+    <div class="polowa">
+        <p>Płeć: <output id="jakaplec">0</output><br>
+        K<input id="plec" type="range" min="-1" max="1" step="0.01" oninput="sliderRefresh()" value=0>M</p>
+        <p>Nazwa użytkownika:<br /><input type='text' id='nazwa' /></p>
+        <p>Hasło:<br /><input type="password" id="haslo" /></p>
+        <p>Potwierdź hasło:<br /><input type="password" id="powtwierdzhaslo" /></p>
+    </div>
+    <button onclick="rejestracja()">Zarejestruj</button>
 </body>
+
 </html>
